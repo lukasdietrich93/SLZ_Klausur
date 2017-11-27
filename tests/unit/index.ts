c4842d8401c1a26b62abab66b90abae7192c026a
Lukas Dietrich <lukasdietrich@netnexus.de>
2017-11-27 16:50:27 +0100

    getting started with unit testing

---
+++ tests/unit/index.ts
@@ -0,0 +1,5 @@
+import { LoginController } from './../../src/controller/LoginController';
+import { expect } from 'chai';
+
+//describe('') 
+//https://journal.artfuldev.com/write-tests-for-typescript-projects-with-mocha-and-chai-in-typescript-86e053bdb2b6
